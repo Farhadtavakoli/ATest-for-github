16208dd50574ed0e38ad197eca7bd1514caf33f9
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2020-10-22 23:29:20 +0200

    Add project files.

---
+++ ATest for github/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ATest_for_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ditt namn: ");
+            string namn = Console.ReadLine();
+            for(int i=0;i<namn.Length;i++)
+            {
+                Console.SetCursorPosition(10 - i, 10+i);
+                Console.Write(namn[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
